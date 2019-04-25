8dfecddbcfe8890ccf17e76e70c36fa38529c8b5
minlucyzhao <minlucyzhao@gmail.com>
2019-04-25 00:16:25 +0000

    Add AJAX function to post form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,32 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+$(document).ready(
+  $('#user_info').submit((e)=>{
+    e.preventDefault();
+    const title=$("#user_info input[name='title']").val();
+    const description=$("#user_info input[name='description']").val();
+    const date=$("#user_info input[name='date']").val();
+    const name=$("#user_info input[name='name']").val();
+    const email=$("#user_info input[name='email']").val();
+    $.ajax({
+    method:'POST',
+    url:'/db',
+    data:{
+      title:title,
+      description:description,
+      date:date,
+      name:name,
+      email:email
+    }})
+  
+  })
+  )
