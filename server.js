00c5d6be3e6481bdc26442e42abc2d43a45e0d20
YIYAO <jianyy1990@hotmail.com>
2019-04-25 15:09:28 +0000

    delete GET

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const bodyParser = require("body-parser");
+const sass = require("node-sass-middleware");
+const app = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require('morgan');
+const knexLogger = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -39,10 +39,65 @@ app.use(express.static("public"));
 app.use("/api/users", usersRoutes(knex));
 
 // Home page
+
+
+//FAKE DB
+const DB = [{
+  id: 1,
+  title: 'light house meeting',
+  description: '',
+  timeManage: [{
+    date: 'April 30',
+    time_range: '1:30 - 2:30',
+  }, {
+    date: 'April 31',
+    time_range: '2:30 - 3:30'
+  },
+  {
+    date: 'April 32',
+    time_range: '4:30 - 5:30'
+  }],
+  name: 'YIYAO',
+  email: 'a@example.com'
+}, {
+  id: 2,
+  title: 'light house meeting2',
+  description: '',
+  timeManage: [{
+    date: 'April 1',
+    time_range: '1:30 - 2:30',
+  }, {
+    date: 'April 2',
+    time_range: '2:30 - 3:30'
+  },
+  {
+    date: 'April 3',
+    time_range: '4:30 - 5:30'
+  }],
+  name: 'LUCY',
+  email: 'b@example.com'
+}]
+//function
+
+// ROUTE
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.post('/db', (req, res) => {
+  const { title, description, date, from_time, to_time, name, email } = req.body
+  DB.push({
+    id: DB.length + 1,
+    title: title,
+    description: description,
+    date: date,
+    name: name,
+    email: email
+
+  })
+
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
