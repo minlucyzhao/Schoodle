fb1d1747c2c1c8a02781b9a9566069056adedc8a
shivangna <shivangna.kaistha@gmail.com>
2019-04-25 22:44:25 +0000

    remove code in app.js and add to server.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,50 +9,50 @@
 //   });;
 // });
 
-$(document).ready(() => {
-  $('#user_info').submit((e) => {
-    console.log('submit works!!')
-    e.preventDefault();
-    const title = $("#user_info input[name='title']").val();
-    const description = $("#user_info input[name='description']").val();
-    const date = $("#user_info input[name='date']").val();
-    const name = $("#user_info input[name='name']").val();
-    const email = $("#user_info input[name='email']").val();
-    $.ajax({
-      method: 'POST',
-      url: '/db',
-      data: {
-        title: title,
-        description: description,
-        date: date,
-        name: name,
-        email: email
-      }
-    })
+// $(document).ready(() => {
+//   $('#user_info').submit((e) => {
+//     console.log('submit works!!')
+//     e.preventDefault();
+//     const title = $("#user_info input[name='title']").val();
+//     const description = $("#user_info input[name='description']").val();
+//     const date = $("#user_info input[name='date']").val();
+//     const name = $("#user_info input[name='name']").val();
+//     const email = $("#user_info input[name='email']").val();
+//     $.ajax({
+//       method: 'POST',
+//       url: '/db',
+//       data: {
+//         title: title,
+//         description: description,
+//         date: date,
+//         name: name,
+//         email: email
+//       }
+//     })
 
-  })
-  ajax_get()
-  timepicker()
-})
-function ajax_get() {
-  $.get('/db', (data, status) => {
-    console.log('get date', data)
-  })
-}
+//   })
+//   ajax_get()
+//   timepicker()
+// })
+// function ajax_get() {
+//   $.get('/db', (data, status) => {
+//     console.log('get date', data)
+//   })
+// }
 
 
-function timepicker() {
-  // initialize input widgets first
-  $('#time-picker .time').timepicker({
-    'showDuration': true,
-    'timeFormat': 'g:ia'
-  });
+// function timepicker() {
+//   // initialize input widgets first
+//   $('#time-picker .time').timepicker({
+//     'showDuration': true,
+//     'timeFormat': 'g:ia'
+//   });
 
-  $('#time-picker .date').datepicker({
-    'format': 'yyyy-m-d',
-    'autoclose': true
-  });
-  // initialize datepair
-  $('#time-picker').datepair();
-}
-//
+//   $('#time-picker .date').datepicker({
+//     'format': 'yyyy-m-d',
+//     'autoclose': true
+//   });
+//   // initialize datepair
+//   $('#time-picker').datepair();
+// }
+// //
