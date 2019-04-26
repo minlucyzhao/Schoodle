b19d8f794fcb5f820b259db86906058366f42d63
shivangna <shivangna.kaistha@gmail.com>
2019-04-26 23:34:32 +0000

    fix index.ejs, removed extra inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,16 +33,8 @@ function showInput() {
     callBack(count)
     count++
   })
-<<<<<<< HEAD
-=======
-
->>>>>>> 9e433f0f99d84ff4e3b2ccb6e5916af7a6ae0d59
 }
 function callBack(count) {
   console.log(count)
   $(`#time-picker span:nth-child(${count})`).show()
-<<<<<<< HEAD
-=======
-
->>>>>>> 9e433f0f99d84ff4e3b2ccb6e5916af7a6ae0d59
 }
