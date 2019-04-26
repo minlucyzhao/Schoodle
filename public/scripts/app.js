fa19f1f06ef51493840a4e6147e41d586652ea17
YIYAO <jianyy1990@hotmail.com>
2019-04-26 19:11:35 +0000

    add meet and success router

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,18 +2,24 @@
 
 $(document).ready(() => {
   timepicker()
+  copyToClipboard()
 })
 
 function timepicker() {
   // initialize input widgets first
-  $('#time-picker .time').timepicker({
-    'showDuration': true,
-    'timeFormat': 'g:ia'
-  });
+  // $('#time-picker .time').timepicker({
+  //   'showDuration': true,
+  //   'timeFormat': 'g:ia'
+  // });
 
   $('#time-picker .date').datepicker({
     'format': 'yyyy-m-d',
     minDate: 0,
     'autoclose': true
   });
+}
+function copyToClipboard() {
+  const $copyText = $('#urls')
+  $copyText.select();
+  document.execCommand("copy");
 }
