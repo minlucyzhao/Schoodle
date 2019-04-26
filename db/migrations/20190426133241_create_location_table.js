514cc3e94862ddc65f132fc4bb56600016216669
minlucyzhao <minlucyzhao@gmail.com>
2019-04-26 14:33:43 +0000

    Add new location table in db

---
+++ db/migrations/20190426133241_create_location_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('locations', function (table) {
+        table.increments();
+        table.string('address');
+        table.integer('longitude');
+        table.integer('latitude');
+        table.integer('user_id');
+        table.integer('event_id');
+        table.foreign('user_id').references('users.id');
+        table.foreign('event_id').references('events.id');
+    });    
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('locations');
+};
